21ff3d8dea992af6c2fc99a7689f2fb201a634ee
hongtayvy <hongtayvy@gmail.com>
2022-03-09 11:56:49 -0600

    chore: example for sean boy

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Practice {
     public static void main(String[]args){
-        System.out.println("practice-victor");
+        List<String> seanList = new ArrayList();
+        List<String> victorList = new ArrayList();
+
+        for(int i = 0; i < 10; i++){
+            seanList.add("sean android #: " + (i + 1)); //add to list
+            victorList.add("victor clone #: " + (i + 1));
+        }
+
+        printItems(seanList);
+        printItems(victorList);
+    }
+
+    public static void printItems(List<String> list){
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i)); //print out right away
+        }
     }
 }
